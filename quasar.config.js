2411f61f1b402e39dcb1252e52ba269491382407
Suchanan0411 <suchanan.8864@gmail.com>
2025-10-08 10:05:38 +0700

    Edited build

--- quasar.config.js
+++ quasar.config.js
@@ -36,10 +36,7 @@ export default defineConfig((/* ctx */) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#build
     build: {
-      target: {
-        browser: [ 'es2022', 'firefox115', 'chrome115', 'safari14' ],
-        node: 'node20'
-      },
+      publicPath: '/id-name-lastname/',
 
       vueRouterMode: 'hash', // available values: 'hash', 'history'
       // vueRouterBase,
