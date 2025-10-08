11042b802d3164e451f7695aae1adc5aa5dff5d0
Suchanan0411 <suchanan.8864@gmail.com>
2025-10-08 11:06:27 +0700

    Edited quasar4

--- quasar.config.js
+++ quasar.config.js
@@ -36,7 +36,7 @@ export default defineConfig((/* ctx */) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#build
     build: {
-      publicPath: '/',
+      publicPath: '6704101376-suchanan-chaweechan',
       vueRouterMode: 'hash', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
